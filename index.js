300b028ecd613d3c53287b04c9ab6a1356df948f
Anca <anca.popescu@metafused.com>
2016-03-19 17:41:12 +0000

    added a message when user connects or disconnects

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  socket.broadcast.emit("chat message", "a user is connecting")
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+    socket.broadcast.emit("chat message", "a user is disconnecting")
+  });
+
 });
 
 http.listen(3000, function(){
